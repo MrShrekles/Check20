5d16a2b494b1a3d57c29ae596f4b4f96e71d1086
MrShrekles <mrshrekles@gmail.com>
2026-06-20 07:36:45 -0700

    lil thing
    hoipe fix

--- active-sheet/service-worker.js
+++ active-sheet/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'arc20-v26';
+const CACHE = 'arc20-v27';
 
 // Everything network-first during active development.
 // When data is stable, data/*.json can be moved back to cache-first for offline use.
